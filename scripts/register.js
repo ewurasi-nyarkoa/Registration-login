a40e7ef6c273e180a8b2ce9bb2da117b2af76564
Ewurasi Ayanful <anewurasi@gmail.com>
2024-09-17 15:55:56 +0000

    worked on my message content

--- scripts/register.js
+++ scripts/register.js
@@ -1,8 +1,24 @@
 // Get form element by id
-const registerForm = document.getElementById('register');
+const registerForm = document.querySelector('#register');
+console.log(registerForm)
 // Handle form submit event
-registerForm.onsubmit = function(event) {
+registerForm.addEventListener('submit', function (event) {
     event.preventDefault();
-    console.log(event);
+    //collect form data
+    const formData = new FormData(registerForm);
+   // console.log(formData.get('Username'), formData.get('email'),formData.get('password'))
+   //send data to backend
+   //display success message to the user
+   const messageH1 = document.querySelector('#message');
+   messageH1.textContent = 'Account registered successfully!';
+    
+});
+//declare variable age
+//let age= 71;
+//define square age function
+function squareAge (age){
+return age ** 2;
+};
 
-}
+let result = squareAge(3)
+console.log(result)
